dbf825b9d2df28c57a7dd35cb8dbbd05aae8e17f
Viktor <victorharasymiv97@gmail.com>
2025-08-06 23:47:02 +0200

    UPD REPO

---
+++ src/server.js
@@ -0,0 +1,56 @@
+// src/server.js
+
+import express from 'express';
+import dotenv from 'dotenv';
+import pino from 'pino-http';
+import cors from 'cors';
+
+import { getEnvVar } from './utils/getEnvVar.js';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+  dotenv.config();
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+  //   app.get('/contacts', (req, res) => {
+  //     status: 200,
+  // 	message: "Successfully found contact with id {contactId}!",
+  // 	data: {
+
+  //       });
+
+  app.use((req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
